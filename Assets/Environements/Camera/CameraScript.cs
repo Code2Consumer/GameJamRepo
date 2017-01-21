e1eea1d0ecbfca30783588a68c2656f147b28b52
aboukhriss.zakaria@hotmail.com <aboukhriss.zakaria@hotmail.com>
2017-01-21 11:20:31 +0100

    Finishing camera script
    Signed-off-by: aboukhriss.zakaria@hotmail.com <aboukhriss.zakaria@hotmail.com>

--- Assets/Environements/Camera/CameraScript.cs
+++ Assets/Environements/Camera/CameraScript.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour {
 
 	public Transform Player;
+	public float height;
+	public float width;
+	public float speed;
 
 	// Use this for initialization
 	void Start () {
 		 RenderSettings.ambientLight = Color.black;
 		// GameObject thePlayer = GameObject.Find("/Player").transform;
 		// Player = thePlayer.Transform;
+		height = Camera.main.orthographicSize *2f;
+        width = height / (float)Screen.height * (float)Screen.width;
+        speed = 10;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(Player.GetComponent<AnimationCharacter>().getLookRight());
-		gameObject.transform.position = new Vector3(Player.position.x+8, Player.position.y, gameObject.transform.position.z);
+		height = Camera.main.orthographicSize *2f;
+        width = height / (float)Screen.height * (float)Screen.width;
+
+        float step = speed * Time.deltaTime;
+		if(Player.GetComponent<AnimationCharacter>().getLookRight()){
+			// gameObject.transform.Translate(new Vector3(Player.position.x+(width*0.4f), Player.position.y, gameObject.transform.position.z).forward );
+			transform.position = Vector3.MoveTowards(transform.position, new Vector3(Player.position.x+(width*0.4f), Player.position.y, gameObject.transform.position.z), step);
+		}else{
+			// gameObject.transform.Translate(new Vector3(Player.position.x+(width*-0.4f), Player.position.y, gameObject.transform.position.z).forward );
+			transform.position = Vector3.MoveTowards(transform.position, new Vector3(Player.position.x, Player.position.y, gameObject.transform.position.z), step);
+		}
 	}
 }
