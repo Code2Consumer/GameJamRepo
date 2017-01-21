247440793b3c80f78f90fd590ae7b1c7a0aa053e
aboukhriss.zakaria@hotmail.com <aboukhriss.zakaria@hotmail.com>
2017-01-21 04:08:47 +0100

    Adding Shout Power
    Signed-off-by: aboukhriss.zakaria@hotmail.com <aboukhriss.zakaria@hotmail.com>

---
+++ Assets/Characters/Player/CrierScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrierScript : MonoBehaviour {
+
+	private float StartPressingShout; //Force du paralax
+	private float ShoutPower; //Force du paralax
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("space")){
+			if(StartPressingShout == 0){
+				StartPressingShout = Time.time;
+			}
+		}else{
+			if(StartPressingShout != 0){
+				ShoutPower = Time.time - StartPressingShout;
+				Debug.Log("shout: "+ShoutPower);
+				// Shout
+				// Spawn Shout at Player
+			}
+			StartPressingShout = 0;
+		}
+
+	}
+}
