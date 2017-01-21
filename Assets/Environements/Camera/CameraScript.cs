3606c12aab1b992f49ed38cf0088899e917fbed0
aboukhriss.zakaria@hotmail.com <aboukhriss.zakaria@hotmail.com>
2017-01-21 10:37:22 +0100

    ajout du debut du code de la cam
    Signed-off-by: aboukhriss.zakaria@hotmail.com <aboukhriss.zakaria@hotmail.com>

--- Assets/Environements/Camera/CameraScript.cs
+++ Assets/Environements/Camera/CameraScript.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
 
+	public Transform Player;
+
 	// Use this for initialization
 	void Start () {
 		 RenderSettings.ambientLight = Color.black;
+		// GameObject thePlayer = GameObject.Find("/Player").transform;
+		// Player = thePlayer.Transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Debug.Log(Player.GetComponent<AnimationCharacter>().LookRight);
+		gameObject.transform.position = new Vector3(Player.position.x+8, Player.position.y, gameObject.transform.position.z);
 	}
 }
