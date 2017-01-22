ed41ad19a0cce7791edbd6b62286f8cf37be51f6
aboukhriss.zakaria@hotmail.com <aboukhriss.zakaria@hotmail.com>
2017-01-22 03:25:10 +0100

    Ajout de la surbrillance des objects
    Signed-off-by: aboukhriss.zakaria@hotmail.com <aboukhriss.zakaria@hotmail.com>

---
+++ Assets/Environements/Decors/surbrillance/SurbrillanceScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurbrillanceScript : MonoBehaviour {
+
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name == "Bruit" || other.gameObject.name == "Bruit(Clone)" ) {
+			gameObject.GetComponent<SpriteRenderer>().enabled = true ;	
+		}
+	}
+
+}
